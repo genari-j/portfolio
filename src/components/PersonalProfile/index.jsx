59b276f97fcda97213f2fe2092f873303b811572
João Victor Genari <victor.olr@hotmail.com>
2023-03-19 20:37:10 -0300

    refactor: adjusting session descriptions

--- src/components/PersonalProfile/index.jsx
+++ src/components/PersonalProfile/index.jsx
@@ -7,7 +7,8 @@ export function PersonalProfile() {
             <h2>Perfil Pessoal</h2>
             <PersonalSection>
                 <img src={PersonalProfilePic} alt="Imagem Representativa" />
-                <p>Tenho habilidades em ferramentas de versionamento, trabalhando com <Span>Git</Span>, <Span>Github</Span> e <Span>Gitlab</Span>. Dessa forma, é notável maior controle e segurança para os projetos de trabalho.</p>
+                <p>Tenho experiência em Frontend, trabalhando com <Span>HTML</Span>, <Span>CSS</Span>, <Span>Bootstrap</Span>, <Span>SCSS</Span>, <Span>SASS</Span>, <Span>Javascript</Span>, <Span>React.js</Span> e <Span>Styled-Components</Span>, o que me permite criar interfaces responsivas, modernas e agradáveis para o usuário.</p>
+                <p>Além disso, tenho habilidades em ferramentas de versionamento, trabalhando com <Span>Git</Span>, <Span>Github</Span> e <Span>Gitlab</Span>. Dessa forma, é notável maior controle e segurança para os projetos de trabalho.</p>
                 <p>Venho expandindo meus conhecimentos em Backend, estudando <Span>Node.js</Span>, <Span>Express.js</Span> e <Span>MySQL</Span>, para me tornar um desenvolvedor mais completo e capaz de criar soluções que englobam tanto o Frontend quanto o Backend.</p>
                 <p>Sou familiarizado com as metodologias ágeis <Span>Scrum</Span> e <Span>Kanban</Span>, o que me permite trabalhar em equipe de forma mais eficiente e entregando valor de forma constante.</p>
                 <p>Também tenho experiência em trabalhar com <Span>API Rest</Span>, <Span>NPM</Span>, <Span>Yarn</Span> e <Span>Vite</Span>, ferramentas essenciais para o desenvolvimento de software moderno.</p>
